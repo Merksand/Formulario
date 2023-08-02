4e554d9bd0f2c9d10c55100a264e55d5242420bd
Merks <72145231+Merksand@users.noreply.github.com>
2023-08-02 12:12:05 -0600

    Actualizar

--- LoginCompleto/Login.php
+++ LoginCompleto/Login.php
@@ -1,8 +1,7 @@
 <?php
-// Inicia la sesión
+
 session_start();
 
-// Conexión a la base de datos (reemplaza con tus credenciales)
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -18,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    // Consulta a la base de datos para verificar las credenciales
+     verificar las credenciales
     $query = "SELECT * FROM clienteregistro WHERE email = '$email' AND password = '$password'";
     $result = mysqli_query($connection, $query);
 
@@ -27,11 +26,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 
     if (mysqli_num_rows($result) == 1) {
-        // Si las credenciales son correctas, inicia sesión y devuelve "success"
+        
         $_SESSION["email"] = $email;
         echo "success";
-    } else {
-        // Si las credenciales son incorrectas, devuelve "error"
+    } else {
         echo "error";
     }
 }
